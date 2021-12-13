ddca629fecc062cec6449affa9c1da2f320b94b5
Oona <oona.laitamaki@tuni.fi>
2021-12-13 18:16:23 +0200

    Enable application to be visible through https connection (#41)

--- src/app.js
+++ src/app.js
@@ -1,27 +1,36 @@
 /*
-**Backendin pohja.
-**Avaa expressin ja lähettää kutsut, joko käyttöliittymälle tai API:lle
+** Base of the backend application
+** Opens express and sends calls to API and user interface
 */
+
 const express = require('express');
 const app = express();
 const api = require('./routers/api');
 const bodyParser = require('body-parser');
 const path = require('path');
+const https = require('https');
+const fs = require('fs');
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 app.use('/', express.static(path.join(__dirname, '/map-app/build')));
-
-
 app.use('/', express.static('public'));
-
 
 app.use('/api/', api);
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/map-app/build/index.html'));
 });
 
-console.log("listening to port 3000")
+console.log("Listening to port 3000");
 app.listen(3000);
+
+var options = {
+  key: fs.readFileSync(path.join(__dirname, 'key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, 'cert.pem'))
+};
+
+console.log("Listening to port 443");
+https.createServer(options, app).listen(443);
