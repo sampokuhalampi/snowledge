3e649e52c1cb0dc119788ff823f1379572a25f5c
Jere Pesälä <j.pesala@protonmail.com>
2022-03-15 12:18:12 +0200

    First iteration of User review updating logic. Update rate is set to 5 sec for fast testing. Known bug is that it wont update segments where is no Paivitykset data.

---
+++ src/UserReviewUpdater.js
@@ -0,0 +1,66 @@
+const cron = require("node-cron");
+const date = require("date-and-time");
+const database = require("./routers/objectRouters/database");
+
+const sqlQuery = (query) => {
+  return new Promise (function (resolve, reject){
+    database.query(query, function (err, results){
+      if (err) {reject(err);}
+      resolve(results);
+    });
+  });
+};
+
+const sqlInsert = (userTime, userSnowType, segment) => {
+  return new Promise (function (resolve, reject){
+    database.query("UPDATE Paivitykset SET Käyttäjä_Aika=?, Käyttäjä_lumilaatu=?  WHERE Segmentti=? order by Aika desc limit 1;",
+      [
+        userTime,
+        userSnowType,
+        segment
+      ],
+      function (err,results){
+        if (err) {reject(err);}
+        resolve(results);
+      });
+  });
+};
+
+function isObjectEmpty(obj) {
+  return Object.keys(obj).length;
+
+}
+
+
+const userReviewUpdater = cron.schedule("*/5 * * * * *", async () => {
+  console.log("Updating user reviews...");
+
+  const segmentCount = await sqlQuery("SELECT ID FROM Segmentit order by ID desc limit 1;").then(function (results) {
+    results = JSON.parse(JSON.stringify(results));
+    return results[0].ID;
+  });
+
+  for (let i=0; i<segmentCount; i++){
+    const userReviewQuery = "SELECT Segmentti, Lumilaatu, Aika FROM KayttajaArviot WHERE Segmentti = " + (i + 1) + " order by Aika desc limit 1;";
+
+    const userReview = await sqlQuery(userReviewQuery).then(function (results){
+      results=JSON.parse(JSON.stringify(results));
+      return results;
+    });
+
+    if (isObjectEmpty(userReview) !== 0){
+      const timeString = date.format(new Date(userReview[0].Aika), "YYYY-MM-DD HH:mm:ss", true);
+      console.log("Updating segment " + (i+1) + "...");
+      await sqlInsert(timeString, userReview[0].Lumilaatu, userReview[0].Segmentti).then(function (){
+        console.log("Segment updated");
+      });
+
+    }
+  }
+
+  console.log("All segments updated");
+});
+
+
+module.exports = userReviewUpdater;
+
--- src/app.js
+++ src/app.js
@@ -3,34 +3,41 @@
 ** Opens express and sends calls to API and user interface
 */
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const api = require('./routers/api');
-const bodyParser = require('body-parser');
-const path = require('path');
-const https = require('https');
-const fs = require('fs');
+const api = require("./routers/api");
+const bodyParser = require("body-parser");
+const path = require("path");
+const https = require("https");
+const fs = require("fs");
+const userReviewUpdater = require("./UserReviewUpdater");
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use('/', express.static(path.join(__dirname, '/map-app/build')));
-app.use('/', express.static('public'));
+app.use("/", express.static(path.join(__dirname, "/map-app/build")));
+app.use("/", express.static("public"));
 
-app.use('/api/', api);
+app.use("/api/", api);
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/map-app/build/index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/map-app/build/index.html"));
 });
 
 console.log("Listening to port 3000");
 app.listen(3000);
 
 var options = {
-  key: fs.readFileSync(path.join(__dirname, 'key.pem')),
-  cert: fs.readFileSync(path.join(__dirname, 'cert.pem'))
+  key: fs.readFileSync(path.join(__dirname, "key.pem")),
+  cert: fs.readFileSync(path.join(__dirname, "cert.pem"))
 };
 
 console.log("Listening to port 443");
 https.createServer(options, app).listen(443);
+
+userReviewUpdater;
+
+
+
+
